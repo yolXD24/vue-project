48dd75245f0d4b6b2f613b6472b6633cca352681
Leonilo Torres <torresle_sd2053@ceaccs.net>
2019-10-15 16:26:58 +0800

    version2

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,17 @@
     <Header v-if="token !== null" />
     <v-content dark id="content">
       <router-view />
-        <v-footer padless absolute inset dense v-if="is_default_password == false">
-          <v-col class="text-center" cols="12">
-            <v-alert
-              id="v-alert"
-              border="left"
-              colored-border
-              type="error"
-              elevation="2"
-            >Note : Password must be Changed!</v-alert>
-          </v-col>
-        </v-footer>
+      <v-footer padless absolute inset dense >
+        <v-col class="text-center" cols="12" v-if="is_default_password === 'true' && token!== null">
+          <v-alert
+            id="v-alert"
+            border="left"
+            colored-border
+            type="error"
+            elevation="2"
+          >Note : Password must be Changed!</v-alert>
+        </v-col>
+      </v-footer>
     </v-content>
   </v-app>
 </template>
@@ -25,6 +25,7 @@
 import Header from "@/components/Menu";
 import Login from "@/views/Login";
 import jwt_decode from "jwt-decode";
+import { type } from 'os';
 
 export default {
   name: "App",
@@ -35,7 +36,7 @@ export default {
   data() {
     return {
       token: null,
-      is_default_password: null
+      is_default_password :null
     };
   },
   mounted() {
