1b7a12d51ab8b8f63d1c742bfe2ad98e80e8187b
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-11-08 21:48:51 +0800

    response layout

---
+++ api/helpers/response.js
@@ -0,0 +1,8 @@
+module.exports= {
+    status: 200,
+    error: { message: null, body: null },
+    data: {
+        message: null,
+        body: { accessToken: null }
+    },
+}
--- api/modules/module.accounts.js
+++ api/modules/module.accounts.js
@@ -1,14 +1,20 @@
 const jwt = require("jsonwebtoken");
 let models = require("../db.model");
+let resStructure = require("../helpers/response");
+
 module.exports = function (res) {
     models.Staffs.find({ admin: false }, { password: 0 }, (err, account) => {
         let token = jwt.sign({ id: account }, "docxpress");
-        res.status(200).send({ accounts: token });
+        resStructure.data.body.accessToken=token
+        res.send(resStructure);
+        console.log(resStructure)
     }).catch(err => {
         if (err) {
+            resStructure.status=503
             res.status(503).json({
                 message: 'Service unavailable'
             });
+            
         }
     });
 }
